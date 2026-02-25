5783092ce80b7346f6c82b7dbd5447798df7ca0b
B14-AINF <B14@academico.unab.ad>
2026-02-25 15:57:08 -0500

    Segundo Ejercicio cuadrante

--- app/src/main/java/me/EliasCruz/MainActivity.kt
+++ app/src/main/java/me/EliasCruz/MainActivity.kt
@@ -135,3 +135,73 @@ fun CheckScreen() {
         )
     }
 }
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun CuadranteScreen() {
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Row(modifier = Modifier.weight(1f)) {
+
+            QuadrantItem(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+
+            QuadrantItem(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+
+            QuadrantItem(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+
+            QuadrantItem(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantItem(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Text(
+            text = description
+        )
+    }
+}
